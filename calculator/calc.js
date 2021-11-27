a594c39fd80e4274b3fd81c7e7229a727888df43
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-27 15:43:54 -0300

    A4-node modules

---
+++ calculator/calc.js
@@ -0,0 +1,9 @@
+var sumFunc = require("./sum");
+var subFunc = require("./sub");
+var multFunc = require("./mult");
+var divFunc = require("./div");
+
+console.log("soma: "+sumFunc(1,2));
+console.log("sub: "+ subFunc(10,5));
+console.log("mult: "+ multFunc(10,5));
+console.log("div: "+ divFunc(10,5));
