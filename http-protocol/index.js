615c57b658e3090aab8cd7ac741e295f7a35dcb0
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-01 22:36:43 -0300

    A08-parameters

--- http-protocol/index.js
+++ http-protocol/index.js
@@ -4,15 +4,19 @@ const app = express();
 
 
 app.get("/", function(req,res){
-    res.send("Index.js Working!")
+    res.send("Index.js Working!");
 })
 
 app.get("/about", function(req,res){
-    res.send("description about!")
+    res.send("description about!");
 })
 
 app.get("/blog", function(req,res){
-    res.send("my blog!")
+    res.send("my blog!");
+})
+
+app.get('/ola/:name/:age/', function(req, res){
+    res.send("<b>Name:</b> " + req.params.name+ " <br><b>Age:</b> "+ req.params.age);
 })
 
 
